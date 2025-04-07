22430f4379267a1c981746930f3fa522909fad10
Marco Dempsey Wright <wrigh4md@mail.uc.edu>
2025-04-06 21:43:03 -0400

    final

--- src/ListMaker.java
+++ src/ListMaker.java
@@ -17,21 +17,43 @@ public class ListMaker {
         do {
             displayMenu();
             cmd = SafeInput.getRegExString(in, menuPrompt, "[aAdDiIpPqQ]");
-            switch (cmd) {
-                case "A":
+            switch (cmd.toLowerCase()) {
                 case "a":
+                    String newItem = SafeInput.getNonZeroLenString(in, "Enter item to add:");
+                    list.add(newItem);
+                    System.out.println("Item added to the list.");
+                    break;
 
-                case "D":
                 case "d":
-
-                case "I":
+                    if (list.isEmpty()) {
+                        System.out.println("The list is empty. Nothing to delete.");
+                        break;
+                    }
+                    printList();
+                    int deleteIndex = SafeInput.getRangedInt(in, "Enter index to delete:", 1, list.size()) - 1;
+                    String removedItem = list.remove(deleteIndex);
+                    System.out.println("Item \"" + removedItem + "\" removed from position " + (deleteIndex + 1));
+                    break;
                 case "i":
-
-                case "P":
+                    if (list.isEmpty()) {
+                        System.out.println("The list is empty. The item will be inserted at position 1.");
+                    } else {
+                        printList();
+                    }
+                    int insertIndex = SafeInput.getRangedInt(in, "Enter index to insert at:", 1, list.size() + 1) - 1;
+                    String insertItem = SafeInput.getNonZeroLenString(in, "Enter item to insert:");
+                    list.add(insertIndex, insertItem);
+                    System.out.println("Item inserted at position " + (insertIndex + 1));
+                    break;
                 case "p":
-
-                case "Q":
+                    printList();
+                    break;
                 case "q":
+                    if (SafeInput.getYNConfirm(in, "Are you sure you want to quit?")) {
+                        running = false;
+                        System.out.println("Exiting ListMaker. Goodbye!");
+                    }
+                    break;
             }
         }while(running);
 
@@ -40,6 +62,30 @@ public class ListMaker {
     }
 
     public static void displayMenu(){
+        System.out.println("\n===== List Maker Menu =====");
+        System.out.println("A - Add Item");
+        System.out.println("D - Delete Item");
+        System.out.println("I - Insert Item");
+        System.out.println("P - Print List");
+        System.out.println("Q - Quit");
+        System.out.println("===========================\n");
+    }
 
+    public static void printList() {
+        if (list.isEmpty()) {
+            System.out.println("The list is empty.");
+        } else {
+            System.out.println("\n--- Current List ---");
+            for (int i = 0; i < list.size(); i++) {
+                System.out.printf("%2d: %s%n", i + 1, list.get(i));
+            }
+            System.out.println("--------------------\n");
+        }
     }
 }
+
+
+
+
+
+
